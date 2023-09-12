1ad4eb25f57b5ea98b8d8864d99782db250afb4c
Thv1364 <144828312+Thv1364@users.noreply.github.com>
2023-09-12 09:43:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-var gh=0;
-
 var audio2=new Audio("https://cdn.glitch.global/207271c9-b003-4a42-a2af-5969bf0c716f/Tool-Pneuma.mp3?v=1694453054293");
-var audio = new Audio('https://cdn.glitch.global/207271c9-b003-4a42-a2af-5969bf0c716f/Tool-Pneuma.mp3?v=1694453054293');
-function play() {
-  if(gh==0)
-    {
-      audio2.play();
-    }
-  
-  
-  audio.play();
-}
+  audio2.play();
